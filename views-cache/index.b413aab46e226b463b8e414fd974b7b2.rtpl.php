392271304a01dac4360ac7631513a9745e29d6de
RogerioDeFaria <royfaria@gmail.com>
2020-12-23 10:45:13 -0300

    Reinstalação

--- views-cache/index.b413aab46e226b463b8e414fd974b7b2.rtpl.php
+++ views-cache/index.b413aab46e226b463b8e414fd974b7b2.rtpl.php
@@ -1,1 +1,46 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?>		<h1>Hello!</h1>
+<?php if(!class_exists('Rain\Tpl')){exit;}?>		<div id="principal"> <!-- Início principal -->
+
+			<div id="conteudo"> <!-- Início conteudo -->
+				<section id="capa"></section>
+				<img src="../res/img/doTerra-bottles.png">
+
+				<section id="postagens">
+					
+					<article id="video">
+						<h3>Vídeo: doTERRA - O que é um Óleo Essencial</h3>
+
+						<iframe width="400" height="224" src="https://www.youtube.com/embed/YoIk18xWxZ4?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+					</article>
+
+				</section>
+			</div> <!-- Fim conteudo -->
+
+			<aside> <!-- Início aside -->
+				
+				<!--<section id="depoimento">-->
+				<section>
+					<img src="../res/img/bg-laranja.png">
+				</section>
+
+				<section id="galeria"> <!-- Início galeria -->
+
+					<a href="">
+						<img src="../res/img/opt-conceitos.jpg" height="67" width="135" alt="Conceitos">
+					</a>
+
+					<a href="">
+						<img src="../res/img/opt-oleos.jpg" height="67" width="135" alt="Óleos">
+					</a>
+
+					<a href="">
+						<img src="../res/img/opt-condicoes.jpg" height="67" width="135" alt="Condicoes">
+					</a>
+
+					<a href="">
+						<img src="../res/img/opt-blends.jpg" height="67" width="135" alt="Blends">
+					</a>
+					
+				</section> <!-- Fim galeria -->
+
+			</aside> <!-- Fim aside -->
